e4ca51859b43190f2f910a892b6a0b565db50950
hagenkellermann <hagen.kellermann@tngtech.com>
2024-01-26 10:14:08 +0100

    small change to trigger

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ new App({
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Gitlab', 'pipeline failed', 'works on my machine!'
+			'Gitlab', 'pipeline failed', 'Works on my machine!!'
 		],
 		// Which CI/CD tools do you use in your project?
 		tools: 'Jenkins',
